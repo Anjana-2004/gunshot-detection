ec5f1102b7e9164840f099435dd5715b64f1b7f7
Anjana Satish <114793245+Anjana-2004@users.noreply.github.com>
2025-09-21 21:06:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from gunshot_detection.predict import predict
-from doa_estimation.gcc_phat_doa import estimate_doa
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import numpy as np
 
+# Your audio file:
 AUDIO_FILE = "simulated_stereo.wav"
 label = predict(AUDIO_FILE)
 print("Detected Sound:", label)
 
 if label == "Gunshot":
+    # estimate the angle
     angle = estimate_doa(AUDIO_FILE)
     print("Estimated DoA:", round(angle, 2), "degrees")
+
+    
+   
+    true_angle = 45.0  
+
+   
+    mae = mean_absolute_error([true_angle], [angle])
+    rmse = np.sqrt(mean_squared_error([true_angle], [angle]))
+
+    print(f"MAE: {mae:.2f} degrees")
+    print(f"RMSE: {rmse:.2f} degrees")
+
+    # Optional: show +/- 10 degree precision claim
+    if abs(angle - true_angle) <= 10:
+        print("Within ±10° precision ✅")
+    else:
+        print("Outside ±10° precision ❌")
